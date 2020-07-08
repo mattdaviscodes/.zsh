3582c0b7fd837cbce5a4de92b02ae74cf571ee5d
Matt Davis <mdavis.tles@gmail.com>
2020-07-08 15:16:32 -0700

    Fix setup script

--- setup.zsh
+++ setup.zsh
@@ -6,11 +6,10 @@
 # This will fail if the script is run from outside of the cloned directory
 ZSH_CONFIG_DIR=$(pwd)
 
+# Create .zshenv if it doesn't exist
 if [ ! -f "$HOME/.zshenv" ]; then
 	mkdir $HOME/.zshenv;	 
 fi
 
+# Append our custom ZDOTDIR to .zshenv
 echo "ZDOTDIR=$HOME/$ZSH_CONFIG_DIR" >> $HOME/.zshenv;
-
-# Create
-mkdir $ZSH_CONFIG_DIR
